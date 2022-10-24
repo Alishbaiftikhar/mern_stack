4ba3618bbfedcf81c757c0202f63d43fa3297097
Alishba Iftikhar <alishbaiftikhar2001@gmail.com>
2022-10-24 13:53:41 +0500

    committed

--- backend/routes/goRoutes.js
+++ backend/routes/goRoutes.js
@@ -1,17 +1,22 @@
-const express=require('express');
-const router=express.Router();
-const{getGoal,setGoal,updateGoal,deleteGoal}=require('../controllers/goalController')
-const {protect}=require('../middleware/authNiddleware')
+const express = require("express");
+const router = express.Router();
+const {
+  getGoal,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+} = require("../controllers/goalController");
+const { protect } = require("../middleware/authNiddleware");
 //as set and get goal have same routes so we can write them in one lin insteadd
 //of separate functions
-router.route('/').get(protect,getGoal).post(protect,setGoal)
+router.route("/").get(protect, getGoal).post(protect, setGoal);
 // router.get('/',getGoals)
 
 // router.post('/',setGoal)
 
 //as put and delete goal have same routes so we can write them in one lin insteadd
 //of separate functions
-router.route('/:id').put(protect,updateGoal).delete(protect,deleteGoal)
+router.route("/:id").put(protect, updateGoal).delete(protect, deleteGoal);
 // router.put('/:id',updateGoal)
 // router.delete('/:id',deleteGoal)
-module.exports=router
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,17 @@
-const express=require('express')
-const dotenv=require('dotenv').config()
-const colors=require('colors')
-var bodyParser = require('body-parser');
-const connectDB=require('./config/db')
-const{errorHandler}=require('./middleware/errorMiddleware')
-const port=process.env.PORT || 5000
-connectDB()
-const app=express()
-app.use(express.json())
-app.use(express.urlencoded({extended:false}))
-app.use(bodyParser.json())
-app.use('/uploads',express.static('uploads'))
-app.use(errorHandler)
-app.listen(port,()=>console.log('server started on port ${port}'))
-app.use('/api/goals',require('./routes/goRoutes'))
-app.use('/api/users',require('./routes/userRoutes'))
-app.use('/api/brands',require('./routes/brandRoute'))
+const express = require("express");
+const dotenv = require("dotenv").config();
+const colors = require("colors");
+var bodyParser = require("body-parser");
+const connectDB = require("./config/db");
+const { errorHandler } = require("./middleware/errorMiddleware");
+const port = process.env.PORT || 5000;
+connectDB();
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use("/uploads", express.static("uploads"));
+app.use(errorHandler);
+app.listen(port, () => console.log("server started on port ${port}"));
+app.use("/api/goals", require("./routes/goRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
